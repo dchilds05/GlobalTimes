139565a105ca7b7833f137d99bdafecdd7a12b24
dechilds1@gmail.com <dechilds1@gmail.com>
2021-07-29 00:01:01 +0200

    signup page done

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {signup} from "./../../service/auth-service"
 import { Link } from 'react-router-dom';
+import photoSlideshow from "./../../middleware/photoSlideshow"
 
 function Signup(props){
 
@@ -30,33 +31,42 @@ function Signup(props){
         .catch(error => console.log(error))
       }
 
-    return(
-        <div>
-        <form onSubmit={handleFormSubmit}>
-          <label>
-          Username:
-            <input type="text" name="username" value={formState.username} onChange={handleChange}/>
-          </label><br></br>
+    useEffect(() => {
+      photoSlideshow();
+    }, [])
 
-        <label>
-          Password:
-            <input type="password" name="password" value={formState.password} onChange={handleChange}/>
-        </label><br></br>
-          
-        <label>
-          Country of residence:
-            <input type="text" name="country" value={formState.passwcountryord} onChange={handleChange}/>
-          </label><br></br>
-   
-          <button type="submit"> Signup </button>
-        </form>
-   
-        <p>
-          Already have an account?
-          <Link to={"/"}> Login</Link>
-        </p>
-   
-      </div>
+    return(
+      <div>
+            <div className="loginMamaDiv">
+                <div id = "loginLeftDiv1"></div>
+                <div className = "loginRightDiv">
+                    <div className = "loginMasterDiv">
+                        <h1 className="loginHead">Signup</h1>
+                        <form className="loginForm" onSubmit={handleFormSubmit}>
+                            <label>
+                                <input class = "indexInput" placeholder = "Username" type="text" name="username" value={formState.username} onChange={handleChange} required/>
+                            </label><br></br><br></br>
+                    
+                            <label>
+                                <input class = "indexInput" placeholder = "Password" type="password" name="password" value={formState.password} onChange={handleChange} required/>
+                            </label><br></br><br></br>
+
+                            <label>
+                                <input class = "indexInput" placeholder = "Country of residence" type="text" name="country" value={formState.country} onChange={handleChange} required/>
+                            </label><br></br><br></br>
+                    
+                            <button class = "loginButton" type="submit"> Submit </button>
+                        </form><br></br>
+
+                        <hr style = {{width: "30%"}}></hr>
+                        <p className = "loginPar">Already have an account?</p>
+                        <div className = "loginFormDiv2">
+                            <Link className = "signupButton" to={'/'} style={{ textDecoration: 'none', color: "black" }}> Login</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
